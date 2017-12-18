425b97fab99beb6e772a2462bff29995559bd917
1221long <1221long@163.com>
2017-12-18 17:34:51 +0800

    ajust tk import

--- LPractice/py_files/GetFolderList/main.py
+++ LPractice/py_files/GetFolderList/main.py
@@ -1,7 +1,7 @@
-
+#coding=utf8
 
 import os
-from tkinter import *
+import tkinter as tk
 import tkinter.messagebox as msgbox
 from tkinter.filedialog import askdirectory
 
@@ -10,36 +10,41 @@ outTxt = "FolderList.txt"
 if(os.path.exists(outTxt)):
     os.remove(outTxt)
 
-class Application(Frame):
+class Application(tk.Frame):
     
     _path = ""
-
     def __init__(self,master=None):
-        Frame.__init__(self, master)
+        super().__init__(master)
         self.pack()
         self.createWidgets()
 
     def createWidgets(self):
-        self.flab = Label(self,text = "目标路径:")
+        self.flab = tk.Label(self,text = "目标路径:")
         self.flab.pack()
-        self.finput = Entry(self, textvariable = self._path)
-        self.finput.pack()
-        self.fsel = Button(self, text = "路径选择", command = self.selectPath)
+        #self.finput = Entry(self, textvariable = self._path)
+        #self.finput.pack()
+        self.fsel = tk.Button(self, text = "路径选择", command = self.selectPath)
         self.fsel.pack()
+        '''
         self.nameInput = Entry(self)
         self.nameInput.pack()
         self.alertButton = Button(self, text='Hello', command=self.hello)
         self.alertButton.pack()
-    
+        '''
+    '''
     def hello(self):
         name = self.nameInput.get() or 'world'
         msgbox.showinfo('message', 'Hello, %s' % name )
+    '''
 
     def selectPath(self):
         self._path = askdirectory()
-        msgbox.showinfo('filePath', '%s' % self._path)
+        self.flab.config(text = "目标路径: " + self._path)
+        #msgbox.showinfo('filePath', '%s' % self._path)
 
-app = Application()
+root = tk.Tk()
+
+app = Application(root)
 
 app.master.title('Hello world')
 
